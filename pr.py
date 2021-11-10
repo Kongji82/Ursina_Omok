f3a796cc575fac29bba92707ba9411345728a99f
lh99j <87519273+lh99j@users.noreply.github.com>
2021-11-10 21:56:42 +0900

    Add files via upload

--- pr.py
+++ pr.py
@@ -1,77 +1,108 @@
-map = [list(map(int, input().split())) for _ in range(10)]
+import numpy as np
 
-cnt = 0 
+map_size = 10
+Omok_map = np.zeros([map_size, map_size])
+
 row = 0 
 col = 0
-ur = 0 
-dr = 0
+down_Diagonal = 0 
+up_Diagonal = 0
+fin = 0
 
-
-
-def check(x, y, num):
-    if map[x][y] != num:
+def is_num(x, y, num):
+    if Omok_map[x][y] != num:
         return False
     else:
         return True
     
 
 
-def finish(i, j, num):
+def game_over(i, j, num):
+    global fin
     print(num)
+    fin = num
 
 
 
-def search12(i, j, num, row, col, ur, dr):
+
+def rule_chek(i, j, num, row, col, down_Diagonal, up_Diagonal):
 
     for k in range(5):
-        if check(i, j + k, num) == False:
+        if is_num(i, j + k, num) == False:
             break
         else:
             row = row + 1
 
     for k in range(5):
-        if check(i + k, j, num) == False:
+        if is_num(i + k, j, num) == False:
             break
         else:
             col = col + 1
 
     for k in range(5):
-        if check(i + k, j + k, num) == False:
+        if is_num(i + k, j + k, num) == False:
             break
         else:
-            dr = dr + 1
+            down_Diagonal = down_Diagonal + 1
 
     for k in range(5):
-        if check(i - k, j + k, num) == False:
+        if is_num(i - k, j + k, num) == False:
             break
         else:
-            ur = ur + 1
+            up_Diagonal = up_Diagonal + 1
 
 
     if row == 5:
-        finish(i, j, num)
+        game_over(i, j, num)
 
     if col == 5:
-        finish(i, j, num)
+        game_over(i, j, num)
 
-    if ur == 5:
-        finish(i, j, num)
+    if down_Diagonal == 5:
+        game_over(i, j, num)
 
-    if dr == 5:
-        finish(i, j, num)
+    if up_Diagonal == 5:
+        game_over(i, j, num)
 
     row = 0 
     col = 0 
-    ur = 0 
-    dr = 0
+    down_Diagonal = 0 
+    up_Diagonal = 0
 
 
-for i in range(10):
-    for j in range(10):
-        if map[i][j] == 1:
-            search12(i, j, 1, row, col ,ur, dr)
-        elif map[i][j] == 2:
-            search12(i, j, 2, row, col ,ur ,dr)
+def who_win():
+    for i in range(10):
+        for j in range(10):
+            if Omok_map[i][j] == 1:
+                rule_chek(i, j, 1, row, col ,down_Diagonal, up_Diagonal)
+            elif Omok_map[i][j] == 2:
+                rule_chek(i, j, 2, row, col ,down_Diagonal ,up_Diagonal)
+
+
+
 
             
+player_turn = map_size * map_size
 
+for i in range(player_turn):
+
+    if fin == 1:
+        print("흑돌이 이겼습니다!")
+        break
+    elif fin == 2:
+        print("백돌이 이겼습니다!")
+        break
+    else:
+        if i % 2 == 0:
+            x, y = map(int, input("흑돌 차례입니다 : ").split())
+            Omok_map[x, y] = 1
+            who_win()
+            
+
+        else: 
+            x, y = map(int, input("백돌 차례입니다 : ").split())
+            Omok_map[x, y] = 2
+            who_win()
+            
+
+    print(Omok_map)
