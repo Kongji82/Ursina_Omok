eaa1efc2a92ee05860d606db113328a9a9bae07b
Kong <kongji4092@gmail.com>
2021-11-11 15:45:48 +0900

    오류부분 수정

--- gui.py
+++ gui.py
@@ -16,6 +16,8 @@ camera.position = (w//2, h//2)
 Entity(model=Grid(w+1, h+1), scale=w+1, color=color.black, x=w//2-0.5, y=h//2-0.5, z=0.1)
 board_buttons = [[None for x in range(w)] for y in range(h)]
 
+global flag
+flag = True
 for y in range(h):
     for x in range(w):
         b = Button(parent=scene, position=(x, y), color=color.clear, model='circle', scale=0.9)
@@ -29,27 +31,31 @@ for y in range(h):
             if b.collision:
                 b.color = color.clear
 
+        # 마우스 커서에 돌 확인
         b.on_mouse_enter = on_mouse_enter
         b.on_mouse_exit = on_mouse_exit
 
-        def b_click(b=b):
+        def click(b=b):
+            global flag
+            if flag == True:
+                b.text = "B"
+                b.color = color.black
+                b.collision = False
+                flag = False
+            else:
+                b.text = "W"
+                b.text_color = color.black
+                b.color = color.white
+                b.collision = False 
+                flag = True
             # black turn
-            # b.text = '1'
-            b.color = color.black
-            b.collision = False
 
-        def w_click(b=b):
-            # white turn
+        # def w_click(b=b):
+        #     # white turn
+        #     b.text = "W"
+        #     b.color = color.white
+        #     b.collision = False
 
-            b.color = color.white
-            b.collision = False
-
-        turn = x + y
-        
-        if turn % 2 == 0:
-            b.on_click = b_click
-        else:
-            b.on_click = w_click
-
+        b.on_click = click
 
 app.run()
