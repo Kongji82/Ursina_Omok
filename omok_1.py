1fbbdab23ed2fb47f2f7e113e860c3e8c7b9a035
Kong <kongji4092@gmail.com>
2021-11-20 20:32:15 +0900

    코드 리팩토링(메인, 세팅)

--- omok_1.py
+++ omok_1.py
@@ -1,10 +1,14 @@
 from re import T
 from ursina import *
+from pr import *
 import numpy as np
 
 app = Ursina()
 
+global flag
+flag = True
 w, h = 20, 20
+board_buttons = [[None for x in range(w)] for y in range(h)]
 
 # 창 설정
 window.borderless = False
@@ -15,83 +19,16 @@ camera.orthographic = True      # 2D
 camera.fov = 23
 camera.position = (w//2, h//2)
 
-Entity(model=Grid(w+1, h+1), scale=w+1, color=color.black, x=w//2-0.5, y=h//2-0.5, z=0.1)   
 map_size = 20
 Omok_map = np.zeros([map_size, map_size])
 
-row = 0 
-col = 0
-down_Diagonal = 0 
-up_Diagonal = 0
-fin = 0
-global flag
-flag = True
-
-
-board_buttons = [[None for x in range(w)] for y in range(h)]
-
-def is_num(x, y, num):
-    if Omok_map[x][y] != num:
-        return False
-    else:
-        return True
-    
-def game_over(i, j, num):
-    global fin
-    fin = num
-
-
-def rule_chek(i, j, num, row, col, down_Diagonal, up_Diagonal):
-    for k in range(5):
-        if is_num(i, j + k, num) == False:
-            break
-        else:
-            row = row + 1
-
-    for k in range(5):
-        if is_num(i + k, j, num) == False:
-            break
-        else:
-            col = col + 1
-
-    for k in range(5):
-        if is_num(i + k, j + k, num) == False:
-            break
-        else:
-            down_Diagonal = down_Diagonal + 1
-
-    for k in range(5):
-        if is_num(i - k, j + k, num) == False:
-            break
-        else:
-            up_Diagonal = up_Diagonal + 1
-
-    
-    if row == 5 or col == 5 or down_Diagonal == 5 or up_Diagonal == 5:
-        game_over(i, j, num)
-
-
-    row = 0 
-    col = 0 
-    down_Diagonal = 0 
-    up_Diagonal = 0
-
-
-def who_win():
-    for i in range(20):
-        for j in range(20):
-            if Omok_map[i][j] == 1:
-                rule_chek(i, j, 1, row, col ,down_Diagonal, up_Diagonal)
-            elif Omok_map[i][j] == 2:
-                rule_chek(i, j, 2, row, col ,down_Diagonal ,up_Diagonal)
-
+Entity(model=Grid(w+1, h+1), scale=w+1, color=color.black, x=w//2-0.5, y=h//2-0.5, z=0.1)   
 
 def show_winner(won_player):
     Panel(z=1, scale=10, model='quad')
     t = Text(f'Player {won_player} won!', scale=3, origin=(0, 0), background=True)
     t.create_background(padding=(.5,.25), radius=Text.size/2)
     b1.text_color = color.clear
-
 
 b1 = Button(text="undo", scale=(0.1, 0.1, 0.1), position = (.6, .3), color = color.clear, model = 'quad')
 
@@ -104,7 +41,6 @@ def _reset_(b1 = b1):
             board_buttons[y][x].collision = True
 
 b1.on_click = _reset_
-
 
 def game_start():
     for y in range(h):
@@ -126,32 +62,29 @@ def game_start():
 
             def click(b=b):
                 count = 1
-                if fin == 1:
-                    show_winner(1)
-                elif fin == 2:
-                    show_winner(2)
+                global flag
+                if flag == True:
+                    b.text = "B"
+                    b.color = color.black
+                    b.collision = False
+                    flag = False
+                    Omok_map[19 - int(b.position.y)][int(b.position.x)] = 1
+                    if who_win(Omok_map) == 1:
+                        show_winner(1)
+                
                 else:
-                    global flag
-                    if flag == True:
-                        b.text = "B"
-                        b.color = color.black
-                        b.collision = False
-                        flag = False
-                        Omok_map[19 - int(b.position.y) ][int(b.position.x)] = 1
-                        who_win()
-                    
-                    else:
-                        b.text = "W"
-                        b.text_color = color.black
-                        b.color = color.white
-                        b.collision = False 
-                        flag = True
-                        Omok_map[19 - int(b.position.y)][int(b.position.x)] = 2
-                        who_win()
+                    b.text = "W"
+                    b.text_color = color.black
+                    b.color = color.white
+                    b.collision = False 
+                    flag = True
+                    Omok_map[19 - int(b.position.y)][int(b.position.x)] = 2
+                    if who_win(Omok_map) == 2:
+                        show_winner(2)
 
             b.on_click = click
 
 
-
-game_start()
-app.run()
+if __name__ == "__main__":
+    game_start()
+    app.run()
--- pr.py
+++ pr.py
@@ -1,108 +1,58 @@
 import numpy as np
 
-map_size = 10
-Omok_map = np.zeros([map_size, map_size])
-
-row = 0 
-col = 0
-down_Diagonal = 0 
-up_Diagonal = 0
-fin = 0
-
-def is_num(x, y, num):
+def is_num(x, y, num, Omok_map):
     if Omok_map[x][y] != num:
         return False
     else:
         return True
-    
 
-
-def game_over(i, j, num):
-    global fin
+def game_over(num):
     print(num)
-    fin = num
+    return num
 
-
-
-
-def rule_chek(i, j, num, row, col, down_Diagonal, up_Diagonal):
-
-    for k in range(5):
-        if is_num(i, j + k, num) == False:
-            break
-        else:
-            row = row + 1
-
-    for k in range(5):
-        if is_num(i + k, j, num) == False:
-            break
-        else:
-            col = col + 1
-
-    for k in range(5):
-        if is_num(i + k, j + k, num) == False:
-            break
-        else:
-            down_Diagonal = down_Diagonal + 1
-
-    for k in range(5):
-        if is_num(i - k, j + k, num) == False:
-            break
-        else:
-            up_Diagonal = up_Diagonal + 1
-
-
-    if row == 5:
-        game_over(i, j, num)
-
-    if col == 5:
-        game_over(i, j, num)
-
-    if down_Diagonal == 5:
-        game_over(i, j, num)
-
-    if up_Diagonal == 5:
-        game_over(i, j, num)
-
+def rule_chek(i, j, num, Omok_map):
     row = 0 
     col = 0 
     down_Diagonal = 0 
     up_Diagonal = 0
 
+    for k in range(5):
+        if is_num(i, j + k, num, Omok_map) == False:
+            break
+        else:
+            row = row + 1
 
-def who_win():
+    for k in range(5):
+        if is_num(i + k, j, num, Omok_map) == False:
+            break
+        else:
+            col = col + 1
+
+    for k in range(5):
+        if is_num(i + k, j + k, num, Omok_map) == False:
+            break
+        else:
+            down_Diagonal = down_Diagonal + 1
+
+    for k in range(5):
+        if is_num(i - k, j + k, num, Omok_map) == False:
+            break
+        else:
+            up_Diagonal = up_Diagonal + 1
+    
+    if row == 5 or col == 5 or down_Diagonal == 5 or up_Diagonal == 5:
+        winner = game_over(num)
+        return winner
+        
+def who_win(Omok_map):
     for i in range(10):
         for j in range(10):
             if Omok_map[i][j] == 1:
-                rule_chek(i, j, 1, row, col ,down_Diagonal, up_Diagonal)
+                winner = rule_chek(i, j, 1, Omok_map)
+                if winner == 1:
+                    return 1
+                    
             elif Omok_map[i][j] == 2:
-                rule_chek(i, j, 2, row, col ,down_Diagonal ,up_Diagonal)
-
-
-
-
-            
-player_turn = map_size * map_size
-
-for i in range(player_turn):
-
-    if fin == 1:
-        print("흑돌이 이겼습니다!")
-        break
-    elif fin == 2:
-        print("백돌이 이겼습니다!")
-        break
-    else:
-        if i % 2 == 0:
-            x, y = map(int, input("흑돌 차례입니다 : ").split())
-            Omok_map[x, y] = 1
-            who_win()
-            
-
-        else: 
-            x, y = map(int, input("백돌 차례입니다 : ").split())
-            Omok_map[x, y] = 2
-            who_win()
-            
-
-    print(Omok_map)
+                winner = rule_chek(i, j, 2, Omok_map)
+                if winner == 2:
+                    return 2
